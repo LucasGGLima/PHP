5f545d6bb9b4a9a1edec0d43e7e9e289f8d10780
LucasGGLima <lucaogg2016@gmail.com>
2024-05-22 16:18:42 -0400

    v6

--- resultado.php
+++ resultado.php
@@ -22,5 +22,16 @@ for($i; $i < 10; $i++){
         $comparacao++;
     }
 }
-echo $comparacao;
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+</head>
+<body>
+   <p> <?php echo($comparacao)?></p>
+</body>
+</html>
